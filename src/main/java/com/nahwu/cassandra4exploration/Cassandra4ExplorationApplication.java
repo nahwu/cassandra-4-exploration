628f8a5c35184e2e2618d1a5b729559cfb14d7db
Nah Wu <nahwu@hotmail.com>
2022-11-08 10:09:33 +0800

    Init

---
+++ src/main/java/com/nahwu/cassandra4exploration/Cassandra4ExplorationApplication.java
@@ -0,0 +1,13 @@
+package com.nahwu.cassandra4exploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cassandra4ExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cassandra4ExplorationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nahwu/cassandra4exploration/Cassandra4ExplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwu.cassandra4exploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cassandra4ExplorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
